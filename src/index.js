372fc377c167842c8a2b0d3dbbcc89cf7feaa520
dmitrymitenkoff <d.mitenkoff@icloud.com>
2021-09-19 19:24:36 +1200

    Added dark mode and JS toggles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-console.log("it's working");
+const icon = document.querySelector('.mode-icon');
+const mode = document.querySelector('.mode');
+const toggles = document.querySelector('.toggles');
 
-//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 - github personal token
+function swapThemeModeText() {
+  if (mode.innerHTML === 'Light') {
+    mode.innerHTML = 'Dark';
+    icon.src = '../assets/icon-moon.svg';
+  } else {
+    mode.innerHTML = 'Light';
+    icon.src = '../assets/icon-sun.svg';
+  }
+}
+
+function toggleDarkMode() {
+  swapThemeModeText();
+
+  const elements = [
+    document.querySelector('body'),
+    document.querySelector('header'),
+    document.querySelector('.search'),
+    document.querySelector('.card'),
+  ];
+  elements.forEach(element => {
+    element.classList.toggle('dark');
+  });
+}
+
+toggles.addEventListener('click', toggleDarkMode);
